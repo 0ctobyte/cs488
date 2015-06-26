b359d63f5cc7e6417fb31abb241adb0dd70d66df
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-06-25 23:32:07 -0400

    Update all the lua files, Add multithreading
    Got rid of all the attenuation bullshit in the lua scripts
    Added multithreading to the rendering code

--- handin/A4/src/a4.cpp
+++ handin/A4/src/a4.cpp
@@ -1,8 +1,22 @@
 #include "a4.hpp"
 #include "image.hpp"
 
+#include <pthread.h>
 #include <cmath>
 #include <algorithm>
+
+struct RenderThreadData {
+  Image& img;
+  int ystart, yskip, width, height;
+  SceneNode* root;
+  Matrix4x4 unproject;
+  Point3D eye;
+  Colour ambient;
+  double ambient_factor;
+  std::list<Light*> lights;
+  int* progress;
+  bool* done;
+};
 
 Matrix4x4 a4_get_unproject_matrix(int width, int height, double fov, double d, Point3D eye, Vector3D view, Vector3D up)
 {
@@ -39,6 +53,9 @@ Colour a4_lighting(const Ray& ray, const Intersection& i, const Light* light, co
   Vector3D normal = i.n;
   const PhongMaterial *material = dynamic_cast<const PhongMaterial*>(i.m);
   
+  // If light source is blocked then just return the ambient light
+  if(blocked) return ambient * material->diffuse();
+  
   // Set up the parameters for the lights
   // Calculate the vector from the surface point to the light source
   Vector3D surface_to_light = light->position - surface_point;
@@ -68,17 +85,15 @@ Colour a4_lighting(const Ray& ray, const Intersection& i, const Light* light, co
   Colour specular = specular_brightness * material->specular() * light->colour;
 
   // Caluclate the reflection color
-  Colour reflection = material->specular() * reflected_colour; 
+  //Colour reflection = material->specular() * reflected_colour; 
 
   // Calculate attenuation factor
   double attenuation = 1.0 / (light->falloff[0] + light->falloff[1]*distance_to_light + light->falloff[2]*(distance_to_light*distance_to_light));
 
-  Colour lighted = ambient * material->diffuse() + (int)(!blocked) * attenuation * (diffuse + specular + reflection);
-
-  return lighted;
+  return ambient * material->diffuse() + attenuation * (diffuse + specular);
 }
 
-Colour a4_trace_ray(const Ray& ray, const SceneNode *root, const Light* light, const Colour& ambient, const Colour& bg, int recurse_level)
+Colour a4_trace_ray(const Ray& ray, const SceneNode *root, const Light* light, const Colour& ambient, int ambient_factor, const Colour& bg, int recurse_level)
 {
   // Test intersection of ray with scene for each light source
   Colour colour = bg;
@@ -103,14 +118,55 @@ Colour a4_trace_ray(const Ray& ray, const SceneNode *root, const Light* light, c
     if(recurse_level > 0) 
     {
       Ray reflected_ray(hit, ray.direction() - 2*ray.direction().dot(i.n)*i.n);
-      reflected_colour = a4_trace_ray(reflected_ray, root, light, ambient, reflected_colour, --recurse_level);
+      reflected_colour = a4_trace_ray(reflected_ray, root, light, ambient, ambient_factor, reflected_colour, --recurse_level);
     }
 
-    // Perform phong shading at intersection point
-    colour = a4_lighting(ray, i, light, ambient, reflected_colour, blocked);
+    // Perform phong shading at intersection point. The ambient factor is essentially 1 / number of lights.
+    // This is so that the ambient light is not added to the final colour multiple times (one time for each light source)
+    colour = a4_lighting(ray, i, light, ambient_factor*ambient, reflected_colour, blocked);
   }
 
   return colour;
+}
+
+void* a4_render_thread(void* data)
+{
+  RenderThreadData d = *static_cast<RenderThreadData*>(data);
+
+  int one_percent = d.width * d.height * 0.01;
+  int pixel_count = 0, percentage = 0;
+
+  for (int y = d.ystart; y < d.height; y += d.yskip) {
+    for (int x = 0; x < d.width; x++) {
+      // Unproject the pixel to the projection plane
+      Point3D pixel (x, y, 0.0);
+      Point3D p = d.unproject * pixel;
+
+      // Create the ray with origin at the eye point
+      Ray ray(d.eye, p-d.eye);
+
+      // Background colour. a4_trace_ray returns this if no intersections
+      Colour bg = ((x+y) & 0x10) ? (double)y/d.height * Colour(1.0, 1.0, 1.0) : Colour(0.0, 0.0, 0.0);
+
+      // Cast rays for each light that is in the scene
+      // Accumulate the colours returned
+      Colour colour(0.0, 0.0, 0.0);
+      for(auto light : d.lights) colour = colour + a4_trace_ray(ray, d.root, light, d.ambient, d.ambient_factor, bg, 1);
+      
+      d.img(x, y, 0) = colour.R();
+      d.img(x, y, 1) = colour.G();
+      d.img(x, y, 2) = colour.B();
+
+      if(++pixel_count >= one_percent)
+      {
+        pixel_count = 0;
+        *d.progress = ++percentage;
+      }
+    }
+  }
+  *d.done = true;
+
+  return NULL;
 }
 
 void a4_render(// What to render
@@ -144,43 +200,56 @@ void a4_render(// What to render
   double d = view.length();
   Matrix4x4 unproject = a4_get_unproject_matrix(width, height, fov, d, eye, view, up);
     
-  // For now, just make a sample image.
-
+  double ambient_factor = 1 / lights.size();
   Image img(width, height, 3);
 
-  int one_percent = width*height*0.01;
-  int pixel_count = 0, percentage = 0;
+  int progress1 = 0, progress2 = 0, progress3 = 0, progress4 = 0;
+  bool done1 = false, done2 = false, done3 = false, done4 = false;
+  RenderThreadData data1 = {img, 0, 4, width, height, root, unproject, eye, ambient, ambient_factor, lights, &progress1, &done1};
+  RenderThreadData data2 = {img, 1, 4, width, height, root, unproject, eye, ambient, ambient_factor, lights, &progress2, &done2};
+  RenderThreadData data3 = {img, 2, 4, width, height, root, unproject, eye, ambient, ambient_factor, lights, &progress3, &done3};
+  RenderThreadData data4 = {img, 3, 4, width, height, root, unproject, eye, ambient, ambient_factor, lights, &progress4, &done4};
 
-  for (int y = 0; y < height; y++) {
-    for (int x = 0; x < width; x++) {
-      // Unproject the pixel to the projection plane
-      Point3D pixel (x, y, 0.0);
-      Point3D p = unproject * pixel;
+  pthread_t t1, t2, t3, t4;
 
-      // Create the ray with origin at the eye point
-      Ray ray(eye, p-eye);
-
-      // Background colour. a4_trace_ray returns this if no intersections
-      Colour bg = ((x+y) & 0x10) ? (double)y/height * Colour(1.0, 1.0, 1.0) : Colour(0.0, 0.0, 0.0);
-
-      // Cast rays for each light that is in the scene
-      // Accumulate the colours returned
-      Colour colour(0.0, 0.0, 0.0);
-      for(auto light : lights) colour = colour + a4_trace_ray(ray, root, light, ambient, bg, 1);
-      
-      img(x, y, 0) = colour.R();
-      img(x, y, 1) = colour.G();
-      img(x, y, 2) = colour.B();
-
-      if(++pixel_count >= one_percent)
-      {
-        pixel_count = 0;
-        std::cout << "progress: " << ++percentage << "% \r" << std::flush;
-      }
-    }
+  int ret = pthread_create(&t1, NULL, a4_render_thread, &data1);
+  if(ret)
+  {
+    std::cerr << "Abort: pthread_create failed with error code: " << ret << std::endl;
+    exit(EXIT_FAILURE);
   }
-    
+
+  ret = pthread_create(&t2, NULL, a4_render_thread, &data2);
+  if(ret)
+  {
+    std::cerr << "Abort: pthread_create failed with error code: " << ret << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  ret = pthread_create(&t3, NULL, a4_render_thread, &data3);
+  if(ret)
+  {
+    std::cerr << "Abort: pthread_create failed with error code: " << ret << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  ret = pthread_create(&t4, NULL, a4_render_thread, &data4);
+  if(ret)
+  {
+    std::cerr << "Abort: pthread_create failed with error code: " << ret << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  while(!done1 || !done2 || !done3 || !done4)
+  {
+    std::cout << "progress: " << progress1+progress2+progress3+progress4 << "% \r" << std::flush;
+  }
   std::cout << std::endl;
+  
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+  pthread_join(t3, NULL);
+  pthread_join(t4, NULL);
 
   img.savePng(filename);
   
